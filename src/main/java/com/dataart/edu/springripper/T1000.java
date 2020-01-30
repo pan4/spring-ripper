5d912ee192e57119e1bd771dbcb9c67beb1306fb
Alexander Panchenko <Alexander.Panchenko@dataart.com>
2020-01-30 17:29:46 +0300

    spring-ripper part 2

---
+++ src/main/java/com/dataart/edu/springripper/T1000.java
@@ -0,0 +1,8 @@
+package com.dataart.edu.springripper;
+
+public class T1000 extends TerminatorQuoter {
+    @Override
+    public void sayQuote() {
+        System.out.println("Я ЖИДКИЙ");
+    }
+}
